448b08f6054089bff54aa189998dd59f9d738274
Wabtey <wabtey@disroot.org>
2023-08-09 00:02:07 +0200

    impl ordianry ml serialization and tests

--- src/lib.rs
+++ src/lib.rs
@@ -15,9 +15,10 @@
 //! Tree structure based on [`Rc<RefCell<Node>>`](https://applied-math-coding.medium.com/a-tree-structure-implemented-in-rust-8344783abd75)
 #![warn(missing_docs)]
 
-use std::{cell::RefCell, rc::Rc};
-
 use log::warn;
+// use serde::ser::{Serialize, SerializeStruct, Serializer};
+use serde::{Deserialize, Serialize};
+use std::{cell::RefCell, collections::BTreeMap, rc::Rc};
 
 // REFACTOR: Merge WorldEvent and TriggerEvent
 
@@ -1236,4 +1237,92 @@ pub fn init_tree_file(s: String, infos: DialogCustomInfos) -> Rc<RefCell<DialogN
     }
 
     root
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+pub struct DialogNodeYML {
+    source: String,
+    content: Content,
+    trigger_event: Vec<String>,
+}
+
+impl DialogNodeYML {
+    pub fn new(source: String, content: Content, trigger_event: Vec<String>) -> Self {
+        DialogNodeYML {
+            source,
+            content,
+            trigger_event,
+        }
+    }
+}
+
+/// TODO: Custom impl Serialization
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+pub enum Content {
+    Choices(Vec<Choice>),
+    Monolog {
+        text: Vec<String>,
+        exit_state: usize,
+    },
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+pub struct Choice {
+    text: String,
+    /// TODO: Custom Generic Type
+    condition: Option<String>,
+    /// if the `exit_state` is not a key from the Map, it's a end node.
+    exit_state: usize,
+}
+
+impl Choice {
+    pub fn new(text: String, condition: Option<String>, exit_state: usize) -> Self {
+        Choice {
+            text,
+            condition,
+            exit_state,
+        }
+    }
+}
+
+// impl Serialize for Choice {
+//     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+//     where
+//         S: Serializer,
+//     {
+//         // 3 is the number of fields in the struct.
+//         let mut state = serializer.serialize_struct("Choice", 3)?;
+//         state.serialize_field("text", &self.text)?;
+//         state.serialize_field("condition", &self.condition)?;
+//         state.serialize_field("exit_state", &self.exit_state)?;
+//         state.end()
+//     }
+// }
+
+/// The generic Type `C` corresponds to your custom struct Condition.
+///
+/// ```rust
+/// use std::collections::BTreeMap;
+///
+/// fn main() -> Result<(), serde_yaml::Error> {
+///     // You have some type.
+///     let mut map = BTreeMap::new();
+///     map.insert("x".to_string(), 1.0);
+///     map.insert("y".to_string(), 2.0);
+///
+///     // Serialize it to a YAML string.
+///     let yaml = serde_yaml::to_string(&map)?;
+///     assert_eq!(yaml, "x: 1.0\ny: 2.0\n");
+///
+///     // Deserialize it back to a Rust type.
+///     let deserialized_map: BTreeMap<String, f64> = serde_yaml::from_str(&yaml)?;
+///     assert_eq!(map, deserialized_map);
+///     Ok(())
+/// }
+/// ```
+pub fn dialog_deserializer(
+    dialog_file: String,
+) -> Result<BTreeMap<usize, DialogNodeYML>, serde_yaml::Error> {
+    let deserialized_map: BTreeMap<usize, DialogNodeYML> = serde_yaml::from_str(&dialog_file)?;
+    Ok(deserialized_map)
 }
---
+++ tests/yaml_serde_test.rs
@@ -0,0 +1,186 @@
+use fto_dialog::*;
+#[cfg(test)]
+use std::collections::BTreeMap;
+
+#[test]
+fn test_yaml_monolog_serialize_1() {
+    let mut map = BTreeMap::new();
+    map.insert(
+        1,
+        DialogNodeYML::new(
+            "Le Pape".to_string(),
+            Content::Monolog {
+                text: vec![String::from("Hello Homie")],
+                exit_state: 2,
+            },
+            vec![],
+        ),
+    );
+
+    let yaml = serde_yaml::to_string(&map).unwrap();
+    // println!("{}", yaml);
+
+    assert_eq!(
+        yaml,
+        "1:
+  source: Le Pape
+  content: !Monolog
+    text:
+    - Hello Homie
+    exit_state: 2
+  trigger_event: []\n"
+            .to_string()
+    )
+}
+
+#[test]
+fn test_yaml_monolog_serialize_2() {
+    let mut map = BTreeMap::new();
+    map.insert(
+        1,
+        DialogNodeYML::new(
+            "The Frog".to_string(),
+            Content::Monolog {
+                text: vec![
+                    String::from("Hello Homie"),
+                    String::from("I mean..."),
+                    String::from("KeroKero"),
+                ],
+                exit_state: 2,
+            },
+            vec![],
+        ),
+    );
+    map.insert(
+        2,
+        DialogNodeYML::new(
+            "Random Frog".to_string(),
+            Content::Monolog {
+                text: vec![String::from("KeroKero")],
+                exit_state: 3,
+            },
+            vec![],
+        ),
+    );
+
+    let yaml = serde_yaml::to_string(&map).unwrap();
+    // println!("{}", yaml);
+
+    assert_eq!(
+        yaml,
+        "1:
+  source: The Frog
+  content: !Monolog
+    text:
+    - Hello Homie
+    - I mean...
+    - KeroKero
+    exit_state: 2
+  trigger_event: []
+2:
+  source: Random Frog
+  content: !Monolog
+    text:
+    - KeroKero
+    exit_state: 3
+  trigger_event: []\n"
+            .to_string()
+    )
+}
+
+#[test]
+fn test_yaml_choices_serialize_1() {
+    let mut map = BTreeMap::new();
+    map.insert(
+        1,
+        DialogNodeYML::new(
+            "The Frog".to_string(),
+            Content::Choices(vec![
+                Choice::new(String::from("Hello HomeGirl"), None, 2),
+                Choice::new(String::from("KeroKero"), None, 3),
+            ]),
+            vec![],
+        ),
+    );
+    map.insert(
+        2,
+        DialogNodeYML::new(
+            "Random Frog".to_string(),
+            Content::Monolog {
+                text: vec![String::from("Yo Homie")],
+                exit_state: 4,
+            },
+            vec![],
+        ),
+    );
+    map.insert(
+        3,
+        DialogNodeYML::new(
+            "Random Frog".to_string(),
+            Content::Monolog {
+                text: vec![String::from("KeroKero")],
+                exit_state: 4,
+            },
+            vec![],
+        ),
+    );
+
+    let yaml = serde_yaml::to_string(&map).unwrap();
+    // println!("{}", yaml);
+
+    assert_eq!(
+        yaml,
+        "1:
+  source: The Frog
+  content: !Choices
+  - text: Hello HomeGirl
+    condition: null
+    exit_state: 2
+  - text: KeroKero
+    condition: null
+    exit_state: 3
+  trigger_event: []
+2:
+  source: Random Frog
+  content: !Monolog
+    text:
+    - Yo Homie
+    exit_state: 4
+  trigger_event: []
+3:
+  source: Random Frog
+  content: !Monolog
+    text:
+    - KeroKero
+    exit_state: 4
+  trigger_event: []\n"
+            .to_string()
+    )
+}
+
+#[test]
+fn test_yaml_monolog_deserialize_field_missing() {
+    let yaml = "1:
+  source: Le Pape
+  content: !Monolog
+    text:
+    - Hello Homie
+    exit_state: 2\n";
+
+    let mut map = BTreeMap::new();
+    map.insert(
+        1,
+        DialogNodeYML::new(
+            "Le Pape".to_string(),
+            Content::Monolog {
+                text: vec![String::from("Hello Homie")],
+                exit_state: 2,
+            },
+            vec![],
+        ),
+    );
+
+    let deserialized_map: BTreeMap<usize, DialogNodeYML> = serde_yaml::from_str(&yaml).unwrap();
+
+    assert_eq!(map, deserialized_map)
+}
